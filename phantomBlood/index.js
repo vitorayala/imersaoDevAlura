26158b70577f90d7561d3f6bb4bd517e103d7b3f
Vitor Ayala <vitor.ayala@etec.sp.gov.br>
2021-09-13 12:03:45 -0300

    Primeira Aula

---
+++ phantomBlood/index.js
@@ -0,0 +1,12 @@
+// Declaração de Variáveis
+var nota1 = 10
+var nota2 = 9
+var nota3 = 8
+var nota4 = 7
+
+// Lógica de Média
+var notaFinal = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log("A nota final é: " + notaFinal)
+
+
